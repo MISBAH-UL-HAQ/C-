f06e31abb142e659415aa531f21c6806f82513e4
MISBAH-UL-HAQ <misbah.26-it-2017@mietjammu.in>
2024-05-26 00:30:43 +0530

    assignment 2

---
+++ Assignment 2/User_Profile/User_Profile/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace UserProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            string name;
+            string email;
+            string phoneNumber;
+
+     
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter your email: ");
+            email = Console.ReadLine();
+
+           
+            Console.Write("Enter your phone number: ");
+            phoneNumber = Console.ReadLine();
+
+         
+            Console.WriteLine("\nUser Profile:");
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Email: {email}");
+            Console.WriteLine($"Phone Number: {phoneNumber}");
+
+            
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
+

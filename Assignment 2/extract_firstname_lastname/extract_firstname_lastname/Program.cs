565a50e9b59b8892a8cc95ae45dd65ef70a18257
MISBAH-UL-HAQ <misbah.26-it-2017@mietjammu.in>
2024-05-26 00:31:56 +0530

    extract name

---
+++ Assignment 2/extract_firstname_lastname/extract_firstname_lastname/Program.cs
@@ -0,0 +1,19 @@
+﻿//extract first name lastname
+        Console.Write("Please enter your full name: ");
+        string fullName = Console.ReadLine();
+
+        string[] nameParts = fullName.Split(' ');
+
+        if (nameParts.Length >= 2)
+        {
+            string firstName = nameParts[0];
+            string lastName = nameParts[nameParts.Length - 1];
+
+            Console.WriteLine($"First Name: {firstName}");
+            Console.WriteLine($"Last Name: {lastName}");
+        }
+        else
+        {
+            Console.WriteLine("Please enter both your first and last names separated by a space.");
+        }
+  

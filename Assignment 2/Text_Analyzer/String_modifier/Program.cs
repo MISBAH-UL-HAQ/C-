1372cce8a5766d2623c061d996766ec1a74d82ea
MISBAH-UL-HAQ <misbah.26-it-2017@mietjammu.in>
2024-05-26 00:32:33 +0530

    text analyzer

---
+++ Assignment 2/Text_Analyzer/String_modifier/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TextAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter a paragraph or text: ");
+            string inputText = Console.ReadLine();
+
+            
+            if (!string.IsNullOrEmpty(inputText))
+            {
+               
+                string[] words = inputText.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+                int totalWords = words.Length;
+                int totalLetters = 0;
+
+               
+                foreach (string word in words)
+                {
+                    totalLetters += word.Length;
+                }
+
+               
+                double averageWordLength = totalWords > 0 ? (double)totalLetters / totalWords : 0;
+
+               
+                Console.WriteLine($"\nText Analysis Report:");
+                Console.WriteLine($"Total number of words: {totalWords}");
+                Console.WriteLine($"Average word length: {averageWordLength:F2}");
+            }
+            else
+            {
+                Console.WriteLine("No text entered. Please enter some text to analyze.");
+            }
+
+            
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
